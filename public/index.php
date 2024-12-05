208b45f5f56b4c25caa1df1af99d5ffb4a6b6b26
lhadi <lhadidib07@gmail.com>
2024-12-06 00:35:48 +0100

    validation form done , manque redirection avec les donne

--- public/index.php
+++ public/index.php
@@ -2,19 +2,28 @@
 
 require_once __DIR__ .'/../core/Application.php';
 require_once __DIR__ .'/../controllers/SiteController.php';
+require_once __DIR__ .'/../controllers/AuthController.php';
+
 
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', "home");
+$app->router->get('/',[Controllers\SiteController::class, 'home']);
 
-$app->router->get('/about', function() {
-    return 'About Us';
-});
+$app->router->get('/about', function() { return 'About Us'; });
 
 $app->router->get('/contact',[Controllers\SiteController::class, 'contact']);
 
 $app->router->post('/contact', [Controllers\SiteController::class, 'handleContact']);
+
+
+
+// authentification
+$app->router->get('/login', [Controllers\AuthController::class, 'login']);
+$app->router->post('/login', [Controllers\AuthController::class, 'handlelogin']);
+
+$app->router->get('/register', [Controllers\AuthController::class, 'register']);
+$app->router->post('/register', [Controllers\AuthController::class, 'handleRegister']);
 
 $app->run();
 
